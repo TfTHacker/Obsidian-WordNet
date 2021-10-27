bf5b7dc4f26d488e141265d535f56317095ed196
CK <69121180+TfTHacker@users.noreply.github.com>
2021-10-27 17:06:24 +0200

    Prep for v. Slash command and templates

--- src/EditSuggest.ts
+++ src/EditSuggest.ts
@@ -1,4 +1,4 @@
-import { Editor, EditorPosition, EditorSuggest, EditorSuggestContext, EditorSuggestTriggerInfo, MarkdownView, Notice, PopoverSuggest, PopoverSuggest, TFile } from "obsidian";
+import { Editor, EditorPosition, EditorSuggest, EditorSuggestContext, EditorSuggestTriggerInfo, MarkdownView, Notice, TFile } from "obsidian";
 import WordNetPlugin from "./main";
 import { Definition } from "./suggester";
 
@@ -10,45 +10,52 @@ export default class TheEditorSuggestor extends EditorSuggest<Definition> {
     constructor(plugin: WordNetPlugin) {
         super(plugin.app);
         this.plugin = plugin;
-        this.pattern =  new RegExp(".*;;(.*)$");; 
+        this.updatePattern();
     }
 
-    onTrigger(cursor: EditorPosition, editor: Editor, file: TFile): EditorSuggestTriggerInfo {
-        const range = editor.getRange( { line: cursor.line, ch: 0 }, { line: cursor.line, ch: cursor.ch } );
+    updatePattern(): void {
+        this.pattern = new RegExp(`.*${this.plugin.settings.slashCommandShortcut}(.*)$`);
+    }
+
+    onTrigger(cursor: EditorPosition, editor: Editor, _file: TFile): EditorSuggestTriggerInfo {
+        if (this.plugin.settings.slashCommandEnabled === false) return;
+        const range = editor.getRange({ line: cursor.line, ch: 0 }, { line: cursor.line, ch: cursor.ch });
         const testResults = this.pattern.exec(range);
-        if(!testResults)
+        if (!testResults)
             return null;
         else {
             const suggestText = testResults[1];
             this.lastEditorSuggestTriggerInfo = {
-                        start:  { line: cursor.line, ch: cursor.ch - suggestText.length - 2 },
-                        end: { line: cursor.line, ch: cursor.ch },
-                        query: testResults[1]
-                    }
+                start: { line: cursor.line, ch: cursor.ch - suggestText.length - this.plugin.settings.slashCommandShortcut.length },
+                end: { line: cursor.line, ch: cursor.ch },
+                query: testResults[1]
+            }
             return this.lastEditorSuggestTriggerInfo;
         }
     }
+
     getSuggestions(context: EditorSuggestContext): Promise<Definition[]> {
         return this.plugin.dictionarySuggestor.query(context.query)
     }
 
     renderSuggestion(item: Definition, el: HTMLElement): void {
-        el.createEl('b', {text: item.Term});
-        el.createEl('br');
+        el.createEl("b", { text: item.Term });
+        el.createEl("br");
         el.appendText(item.Definition);
     }
 
     selectSuggestion(item: Definition, evt: MouseEvent | KeyboardEvent): void {
         const currentView = this.plugin.app.workspace.getActiveViewOfType(MarkdownView);
         this.close();
-        if(evt.ctrlKey) {
-            new Notice(item.Term + ' \n' + item.Definition, 60000);
-            currentView.editor.replaceRange('', 
+        if (evt.ctrlKey) {
+            new Notice(item.Term + " \n" + item.Definition, 60000);
+            currentView.editor.replaceRange("",
                 this.lastEditorSuggestTriggerInfo.start,
                 this.lastEditorSuggestTriggerInfo.end);
         }
         else
-            currentView.editor.replaceRange('**' + item.Term + '**\n' + item.Definition + '\n', 
+            currentView.editor.replaceRange(
+                this.plugin.renderDefinitionFromTemplate(item.Term, item.Definition),
                 this.lastEditorSuggestTriggerInfo.start,
                 this.lastEditorSuggestTriggerInfo.end);
     }
--- src/suggester.ts
+++ src/suggester.ts
@@ -1,4 +1,4 @@
-import { DataAdapter, FuzzyMatch, FuzzySuggestModal, MarkdownView, Notice } from 'obsidian';
+import { DataAdapter, FuzzyMatch, FuzzySuggestModal, MarkdownView, Notice, request } from 'obsidian';
 import WordNetPlugin from './main';
 
 export interface Definition {
@@ -34,18 +34,13 @@ export default class DictionarySuggester extends FuzzySuggestModal<Definition>{
                 } else {
                     const downloadMessage = new Notice("WordNet dictionary is being downloaded, this may take a few minutes. This message will disappear when the process is complete.", 0);
                     try {
-                        const response = await fetch('https://wordnet.glitch.me/dict-WordNet.json');
+                        const response = await request({ url: 'https://github.com/TfTHacker/Obsidian-WordNet/releases/download/WordNetJson/dict-WordNet.json' });
                         downloadMessage.hide();
-                        if (!response.ok) {
-                            if(response.status === 404) {
-                                new Notice(`The WordNet dictionary file is not currently available for download. Please try again later or contact the developer on Twitter: @TfThacker for support.`,30000);
-                            } else {
-                                console.log(`Error in WordNet dictinary: ${response.status}`);
-                                new Notice(`An error has occured with the download, please try again later: ${response.status}`);    
-                            }
+                        if ( response === "Not Found" || response === `{"error":"Not Found"}`)  {
+                            new Notice(`The WordNet dictionary file is not currently available for download. Please try again later or contact the developer on Twitter: @TfThacker for support.`,30000);
                             this.plugin.unload();
                         } else {
-                            this.wordNet = await response.json();
+                            this.wordNet = await JSON.parse(response);
                             await adapter.write(pathWordNetJson, JSON.stringify(this.wordNet));
                         }                        
                     } catch (e) {
@@ -66,12 +61,12 @@ export default class DictionarySuggester extends FuzzySuggestModal<Definition>{
     }
 
     query(term: string): any {
-        let results = [];
+        const results = [];
         const searchTerm = term.toLocaleLowerCase();
         let countOfFoundMatches = 0;
         if (this.customDict != null) {
             for (let i = 0; (i < this.customDict.length && countOfFoundMatches < 30); i++) {
-                let item = this.customDict[i];
+                const item = this.customDict[i];
                 if (item['SearchTerm'].startsWith(searchTerm)) {
                     results.push(this.customDict[i]);
                     countOfFoundMatches++;
@@ -80,7 +75,7 @@ export default class DictionarySuggester extends FuzzySuggestModal<Definition>{
         }
         countOfFoundMatches = 0;
         for (let i = 0; (i < this.wordNet.length && countOfFoundMatches < 20); i++) {
-            let item = this.wordNet[i];
+            const item = this.wordNet[i];
             if (item['SearchTerm'].startsWith(searchTerm)) {
                 results.push(this.wordNet[i]);
                 countOfFoundMatches++;
@@ -101,18 +96,19 @@ export default class DictionarySuggester extends FuzzySuggestModal<Definition>{
                 this.inputEl.setSelectionRange(0, searchTerm.length);
             }
         } else
-            searchTerm = this.inputEl.value.trim()
+            searchTerm = this.inputEl.value.trim();
 
-        return searchTerm === '' ? [] : this.query(searchTerm)
-    };
+        return searchTerm === '' ? [] : this.query(searchTerm);
+    }
 
-    getItemText(item: Definition) {
+    getItemText(item: Definition): string {
         return item.SearchTerm;
     }
 
-    onChooseItem(item: Definition, evt: MouseEvent | KeyboardEvent) { }
+    // @ts-ignore
+    onChooseItem(item: Definition, evt: MouseEvent | KeyboardEvent): void { }
 
-    renderSuggestion(item: FuzzyMatch<Definition>, el: HTMLElement) {
+    renderSuggestion(item: FuzzyMatch<Definition>, el: HTMLElement): void {
         el.createEl('b', {text: item.item.Term});
         el.createEl('br');
         el.appendText(item.item.Definition);
@@ -121,7 +117,7 @@ export default class DictionarySuggester extends FuzzySuggestModal<Definition>{
     onChooseSuggestion(item: FuzzyMatch<Definition>, evt: MouseEvent | KeyboardEvent): void {
         const currentView: any = this.plugin.app.workspace.getActiveViewOfType(MarkdownView);
         if (currentView != null && currentView.getMode() === 'source') 
-            currentView.editor.replaceSelection('**' + item.item.Term + '**\n' + item.item.Definition + '\n\n');
+            currentView.editor.replaceSelection( this.plugin.renderDefinitionFromTemplate(item.item.Term, item.item.Definition) );
         else
             new Notice(item.item.Term + ' \n' + item.item.Definition, 10000);
     }
